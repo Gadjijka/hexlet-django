991d1ea3772b35af0751719333b2914335492338
Gadjijka <galimovgadjimurad2020@gmail.com>
2024-01-12 18:57:17 +0300

    The third lesson

--- hexlet_django_blog/article/views.py
+++ hexlet_django_blog/article/views.py
@@ -3,4 +3,9 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse('article')
+    tags = ['обучение', 'программирование', 'python', 'oop']
+    return render(
+        request,
+        'articles.html',
+        context={'tags': tags},
+    )
